6bbdaf403bee0971547c9d60d51b12569d5e90ed
Mandhir Dua <madua@DELC02L804EFFT0.sea.corp.expecn.com>
2015-05-19 15:42:27 +0530

    delete change added

---
+++ Project1-NYC_subway_data/ProblemSet-1_to_5/Problem3 - Analyzing Subway data/6 - Plotting Residuals.py
@@ -0,0 +1,28 @@
+Python 3.4.2 (v3.4.2:ab2c023a9432, Oct  5 2014, 20:42:22) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+
+>>> import numpy as np
+import scipy
+import matplotlib.pyplot as plt
+
+def plot_residuals(turnstile_weather, predictions):
+    '''
+    Using the same methods that we used to plot a histogram of entries
+    per hour for our data, why don't you make a histogram of the residuals
+    (that is, the difference between the original hourly entry data and the predicted values).
+    Try different binwidths for your histogram.
+
+    Based on this residual histogram, do you have any insight into how our model
+    performed?  Reading a bit on this webpage might be useful:
+
+    http://www.itl.nist.gov/div898/handbook/pri/section2/pri24.htm
+    '''
+    
+    plt.figure()
+    (turnstile_weather["ENTRIESn_hourly"] - predictions).hist()
+    return plt
+
+
