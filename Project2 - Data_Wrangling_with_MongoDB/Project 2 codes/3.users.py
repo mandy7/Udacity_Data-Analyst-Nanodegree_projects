d131e533e0ee94deb6036186da2f5f74af158f74
mandy7 <mandhir.dua1988@gmail.com>
2015-07-27 13:10:44 +0530

    Project 2 codes and final report

---
+++ Project2 - Data_Wrangling_with_MongoDB/Project 2 codes/3.users.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import xml.etree.ElementTree as ET
+import pprint
+
+import os
+#Set the proper current working directory
+os.getcwd()
+os.chdir('C:/Users/madua/Project2')
+
+def process_map(filename):
+    users = set()
+    for _, element in ET.iterparse(filename):
+        try:
+            users.add(element.attrib['uid'])
+        except KeyError:
+            pass
+        element.clear() #to clear memory
+    return users
+
+users = process_map('GVRD - Vancouver - OSM XML Raw.osm')
+pprint.pprint(users)
+#RESULT set([])
+#no users have contributed
+#however attributed, created_by, and source may have some useful information by examining the OSM file
